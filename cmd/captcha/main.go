b21578aae8941d4cf2701ac2c424724234207878
TonsonP <tonsonpraphabkul@gmail.com>
2026-05-06 15:12:04 +0700

    feat: add logging for captcha

--- cmd/captcha/main.go
+++ cmd/captcha/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"log/slog"
 	"os"
@@ -15,16 +16,43 @@ import (
 	captchaapp "todoe/internal/captcha/application"
 	captchadomain "todoe/internal/captcha/domain"
 	"todoe/internal/event"
+	"todoe/internal/messaging"
 )
+
+type multiPublisher struct{ publishers []event.Publisher }
+
+func (m *multiPublisher) Publish(ctx context.Context, e event.Event) {
+	for _, p := range m.publishers {
+		p.Publish(ctx, e)
+	}
+}
 
 func main() {
 	mongoURI := os.Getenv("MONGO_URI")
 	if mongoURI == "" {
 		mongoURI = "mongodb://root:root@localhost:27017"
 	}
+
 	port := os.Getenv("CAPTCHA_PORT")
 	if port == "" {
 		port = "3010"
+	}
+
+	amqpURL := os.Getenv("AMQP_URL")
+	if amqpURL == "" {
+		amqpURL = "amqp://guest:guest@localhost:5672/"
+	}
+
+	conn, ch, err := messaging.Connect(amqpURL)
+	if err != nil {
+		log.Fatal("rabbit:", err)
+	}
+	defer conn.Close()
+
+	if err := messaging.DeclareTopology(ch, []messaging.Binding{
+		{Exchange: messaging.CaptchaVerify, Queue: messaging.QueueAuditCaptchaEvents},
+	}); err != nil {
+		log.Fatal("rabbit topology:", err)
 	}
 
 	clientIO := mo.NewIOEither(func() (*mongo.Client, error) {
@@ -32,12 +60,19 @@ func main() {
 	})
 
 	bus := event.NewEventBus()
+
 	repo := captchaadapter.NewMongoRepository(clientIO)
 	projection := captchaadapter.NewProjectionHandler(repo)
+
 	bus.Subscribe(captchadomain.EventIssued, projection)
 	bus.Subscribe(captchadomain.EventVerified, projection)
 
-	service := captchaapp.NewService(repo, bus)
+	publisher := &multiPublisher{publishers: []event.Publisher{
+		bus,
+		messaging.NewPublisher(ch, messaging.CaptchaVerify),
+	}}
+
+	service := captchaapp.NewService(repo, publisher)
 	handler := captchahttp.NewHandler(service)
 
 	app := fiber.New()
